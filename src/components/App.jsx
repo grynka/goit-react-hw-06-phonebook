7a919153517e702cf8390a79bb404f84e8cf9e1f
grynka <ihor.kozhemyakin@gmail.com>
2022-12-26 21:26:47 +0200

    restart

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,61 +1,16 @@
-import Filter from "./Filter/Filter";
-import ContactForm from './ContactForm/ContactForm';
-import ContactList from "./Contactlist/ContactList";
-import { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
-
-
-export default function App() {
-const [contacts, setContacts] = useState(() => {
-  const savedContacts = localStorage.getItem('contacts');
-    if (savedContacts !== null) {
-      return JSON.parse(localStorage.getItem('contacts'))
-      }
-      return []
-})
-const [filter, setFilter] = useState('')
-const normalizedFilter = filter.toLowerCase();
-const filterContacts = contacts.filter(contact =>
-contact.name.toLowerCase().includes(normalizedFilter))
-
-const MyComponent = () => {
-  // Отримуємо необхідну частину стану
-  const value = useSelector(state => state.some.value);
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
+  );
 };
-
-
-useEffect(() => {
-  localStorage.setItem('contacts', JSON.stringify(contacts))
-}, [contacts])
-
-const formSubmitHandler = data => {
-    const contactsName = (contacts.map(contact => contact.name))
-    if (contactsName.includes(data.name)) {
-      alert(data.name + ' is allready in contact');
-    } else
-      setContacts( state => [...state, data] );
-    }
-
- const changeFilter = event => {
-    setFilter(event.currentTarget.value)
-  };
-
- const deleteContact = contactId => {
-    setContacts(
-     contacts.filter(contact => contact.id !== contactId)
-    )
-  };
-
-
-return (
-      <>
-        <h2>Phonebook</h2>
-        <ContactForm onSubmit={formSubmitHandler} />
-        <h2>Contacts</h2>
-        <Filter value={filter} onChange={changeFilter} />
-        <ContactList contacts={filterContacts} onClick={deleteContact} />
-      </>
-    );
-}
-
-
